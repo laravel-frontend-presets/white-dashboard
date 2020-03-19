898fbbead1722cf294c72a5a6c7714ed2c4d7679
AndreiDecuseara <andreidecuseara@ymail.com>
2020-03-19 17:19:50 +0200

    Update Laravel 7.x

--- src/white-stubs/resources/views/layouts/navbars/sidebar.blade.php
+++ src/white-stubs/resources/views/layouts/navbars/sidebar.blade.php
@@ -71,7 +71,7 @@
                     <p>{{ _('RTL Support') }}</p>
                 </a>
             </li>
-            <li class=" {{ $pageSlug == 'upgrade' ? 'active' : '' }}">
+            <li class=" {{ $pageSlug == 'upgrade' ? 'active' : '' }} bg-info">
                 <a href="{{ route('pages.upgrade') }}">
                     <i class="tim-icons icon-spaceship"></i>
                     <p>{{ _('Upgrade to PRO') }}</p>
